8d69a400d002f3128c3143add1260b64ea3493a0
Mattx2k1 <44537080+Mattx2k1@users.noreply.github.com>
2021-12-12 12:11:24 -0500

    TestTaskTwo passing 10/19 tests. Up to Data layer of pt2

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,20 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+    @NotBlank(message ="Name cannot be null")
+    @Size(min = 1, max = 255, message ="Enter appropriate name length(1 - 255 chars)")
+    private String location;
 
+    public Employer() {
 
+    }
+
+    public Employer(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,18 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    public String description;
+
+    public Skill() {
+
+    }
+
+    public Skill(String description) {
+        this.description = description;
+    }
 
 }
